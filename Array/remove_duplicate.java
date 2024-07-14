bb811058c75e143629b7ff826564ae2004ef87af
Ankur4600 <tinkushrivastav78@gmail.com>
2024-07-14 12:52:55 +0530

    moved to Array

--- Array/remove_duplicate.java
+++ Array/remove_duplicate.java
@@ -11,17 +11,19 @@ public class remove_duplicate {
         for(int i=0; i<size;i++){
             array[i]=obj.nextInt();
         }
-        // int new_array[]=new int[size];
-        int tmp=array[0];
-        // new_array[0]=tmp;
+        int new_array[]=new int[size];
+        // int tmp=array[0];
+        int tmp=new_array[0];
         for(int i=1;i<size;i++){
             if(tmp!=array[i]&&tmp<array[i]){
-                System.out.print(tmp);// printing the element of array
-                tmp=array[i];
-                // new_array[i]=tmp;
+                // tmp=array[i];
+                new_array[i]=array[i];
+                tmp=new_array[i];
             }
         }
-        System.out.println(tmp);//printing last element of array
+        for(int i=0;i<new_array.length;i++){
+        System.out.print(new_array[i]);
+        };//printing last element of array
         // System.out.println("this is he new array");
         // for(int i=0; i<size; i++){
         //     System.out.print(new_array[i]);
