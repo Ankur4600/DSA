af2ad72ed2b1c9cef184a31b040a91b22a6723f3
Ankur4600 <tinkushrivastav78@gmail.com>
2024-07-15 16:42:38 +0530

    searching an element in rotated sorted array where dupliucation is present (problem by leetcode)

--- search_algo/search_rotated_sorted_array.java
+++ search_algo/search_rotated_sorted_array.java
@@ -20,12 +20,41 @@ public class search_rotated_sorted_array{
         //in case when element not found in the array
         return -1;
     }
+    /* searching for the element in rotated sorted when duplicates are present*/
+    static boolean search_rotated_sorted_arrayII(int arr[],int target){
+        int low=0, high=arr.length-1, mid=0;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(arr[mid]==target) return true;
+            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+                low=low+1;
+                high=high-1;
+                continue;
+            }
+            else if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && arr[mid]>=target) high=mid-1;
+                else low=mid+1;
+            }
+            else{
+                if(arr[mid]<=target && arr[high]>=target) low=mid+1;
+                else high=mid-1;
+            }
+
+        }
+       return false;
+    }
     public static void main(String[] args) {
-        int array[]={7,8,9,1,2,3,4,5,6};
+       /*  int array[]={7,8,9,1,2,3,4,5,6};
         int target=1;
         int ans=search(array,target); //calling the funtion
         if(ans==-1)System.out.print("The element is not present in the array");
        else System.out.print("The index number where element present is "+ans);
+         */
+       //array with duplicate element
+       int arr[]={6,6,7,8,1,2,3,3,4,5,6};
+       int target2=6;
+       boolean ans2=search_rotated_sorted_arrayII(arr,target2);
+       System.out.print(ans2);
         
     }
 }
